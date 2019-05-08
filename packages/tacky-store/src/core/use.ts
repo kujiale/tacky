25d75dc5ca9efaac7f4846a9994941417ac97e25
feifan <feifan@qunhemail.com>
2019-05-08 13:10:21 +0800

    style: code style fix

--- packages/tacky-store/src/core/use.ts
+++ packages/tacky-store/src/core/use.ts
@@ -2,31 +2,31 @@ import { Middleware, Store } from '../interfaces';
 import { compose } from '../utils/compose';
 import { fail } from '../utils/error';
 
-export const middlewares: Array<Middleware> = []
+export const middlewares: Array<Middleware> = [];
 
 export function use(middleware: Middleware | Array<Middleware>) {
-  const arr = Array.isArray(middleware) ? middleware : [middleware]
-  middlewares.push(...arr)
+  const arr = Array.isArray(middleware) ? middleware : [middleware];
+  middlewares.push(...arr);
 }
 
 export function applyMiddleware() {
   return (createStore: any): Store => {
-    const store = createStore()
+    const store = createStore();
     let dispatch: any = () => {
       fail(`Dispatching while constructing your middleware is not allowed. ` +
         `Other middleware would not be applied to this dispatch.`);
-    }
+    };
     const middlewareAPI = {
       getState: store.getState,
       dispatch: (...args: any[]) => dispatch(...args)
-    }
-    const chain = middlewares.map(middleware => middleware(middlewareAPI))
+    };
+    const chain = middlewares.map(middleware => middleware(middlewareAPI));
 
-    dispatch = compose(...chain)(store.dispatch)
+    dispatch = compose(...chain)(store.dispatch);
 
     return {
       ...store,
       dispatch
-    }
+    };
   }
 }
