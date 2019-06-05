2258bc9154d9dfea7739c6f4d6d360274a61ebba
feifan <feifan@qunhemail.com>
2019-06-05 16:24:32 +0800

    fix: enhance init and render

--- packages/tacky-store/src/components/render.tsx
+++ packages/tacky-store/src/components/render.tsx
@@ -1,14 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { applyMiddleware, use } from '../core/use'
-import { createStore } from '../core/store'
-import effectMiddleware from '../middlewares/effect'
-import loggerMiddleware from '../middlewares/logger'
-import { ctx } from '../const/config'
-import { compose } from '../utils/compose';
-import TackyStoreTree from '../core/stateTree';
-
-let started: boolean = false;
+import { init, isRunning } from '../core/init';
 
 /**
  * Includes render on dom, init built-in middleware, create store, load domain global state and so on.
@@ -18,31 +10,9 @@ export function render(
   querySelector: string,
   callback?: () => void
 ): React.ReactElement<any> {
-  if (!started) {
-    // init built-in middleware
-    if (ctx.middleware.effect) {
-      use(effectMiddleware);
-    }
-
-    if (ctx.middleware.logger) {
-      use(loggerMiddleware);
-    }
-
-    const enhancers = [applyMiddleware()];
-    let composeEnhancers = compose;
-
-    if (process.env.NODE_ENV !== 'production') {
-      // Tacky dev tools extension support.
-    }
-
-    const enhancer = composeEnhancers(...enhancers);
-
-    createStore(enhancer);
-    // load all domain global state from instance
-    TackyStoreTree.loadAll();
+  if (!isRunning) {
+    init();
   }
-
-  started = true;
 
   ReactDOM.render(component, document.querySelector(querySelector) as HTMLElement, callback);
 
--- packages/tacky-store/src/core/init.ts
+++ packages/tacky-store/src/core/init.ts
@@ -5,11 +5,17 @@ import loggerMiddleware from '../middlewares/logger'
 import { ctx } from '../const/config'
 import { compose } from '../utils/compose';
 import TackyStoreTree from '../core/stateTree';
+import { invariant } from '../utils/error';
 
+export let isRunning = false;
 /**
  * Includes init built-in middleware, create store, load domain global state and so on.
  */
 export function init() {
+  invariant(!isRunning, 'Cannot init store multiple times.');
+
+  isRunning = true;
+
   if (ctx.middleware.effect) {
     use(effectMiddleware);
   }
